cd1f80e332316397cb84c1b9293b3235ec89f205
melodia-dev <melodiamirko@gmail.com>
2026-06-09 20:40:57 +0200

    prova proxy cordetabs

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const PROXY_SERVERS = [
 
       'https://api.codetabs.com/v1/proxy?quest=',
         // Alternative:
-
+        
+        'https://api.codetabs.com/v1/proxy?quest=<url_to_http_resource>',
          'https://proxy.cors.sh/', 
          'https://api.allorigins.win/get?url=',           
          'https://api.allorigins.win/raw?url=',           
